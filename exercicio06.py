9109f7ca38113fd2af22402ef6f74191a0eeff2d
Tamirys Hariadny Camargo Couto <tamirys.couto@escola.pr.gov.br>
2025-05-29 14:54:51 +0000

    fiz até o exercicio 07

--- exercicio06.py
+++ exercicio06.py
@@ -1,3 +1,8 @@
 # Bianca está programando o controle de acesso a uma plataforma que só funciona entre 9h e 21h.
 # O programa deve receber a hora atual (formato 24h) e informar se o acesso é permitido.
+horariodefuncionamento = int(input("Digite o horário que você está acessando a plataforma"))
 
+if horariodefuncionamento < 21:
+    print("A plataforma está funcionando perfeitamente.")
+else:
+    print("A plataforma está fora do horário de funcionamento.")
--- exercicio07.py
+++ exercicio07.py
@@ -4,5 +4,17 @@
 # Aprovado (>=7)
 # Em treinamento (>=5 e <7)
 # Reprovado (<5)
+media01 = int(input("Digite o valor da média da avaliação"))
+media02 = int(input("Digite o valor da média da avaliação"))
+media03 = int(input("Digite o valor da média da avaliação"))
+
+if valor >= 7 :
+    print("Você foi aprovado!")
+elif valor >= 5 :
+    print("Você está em treinamento")
+elif valor < 7 :
+    print("Você está em treinamento.")
+else:
+    print("Você foi aprovado!")
 
 
